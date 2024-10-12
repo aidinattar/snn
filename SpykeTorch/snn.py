43e0f21d163a4cb8117e98b023c8a1760d119dce
aidinattar <attaraidin@gmail.com>
2024-10-12 12:46:32 +0200

    add linear spiking layer and modify stdp rule

--- SpykeTorch/snn.py
+++ SpykeTorch/snn.py
@@ -76,6 +76,77 @@ class Convolution(nn.Module):
     def forward(self, input):
         return fn.conv2d(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
+class Linear(nn.Module):
+    r"""Performs a linear transformation over an input spike-wave.
+
+    The input is a 3D tensor with the size :math:`(T, C_{{in}}, N)` where:
+    - :math:`T` is the number of time steps.
+    - :math:`C_{{in}}` is the number of input neurons (channels/units).
+    - :math:`N` is the size of the input layer (the number of features).
+
+    The corresponding output is of size :math:`(T, C_{{out}}, N_{{out}})`, where:
+    - :math:`C_{{out}}` is the number of output neurons (output channels).
+    - :math:`N_{{out}}` is the size of the output layer (the number of output features).
+
+    Args:
+        in_features (int): Number of input features (neurons).
+        out_features (int): Number of output features (neurons).
+        weight_mean (float, optional): Mean of the initial random weights. Default: 0.8.
+        weight_std (float, optional): Standard deviation of the initial random weights. Default: 0.02.
+    """
+    def __init__(self, in_features, out_features, weight_mean=0.8, weight_std=0.02):
+        super(Linear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        # Parameters
+        self.weight = Parameter(torch.Tensor(self.out_features, self.in_features))
+        self.weight.requires_grad_(False)  # We do not use gradients in this spiking layer
+        self.reset_weight(weight_mean, weight_std)
+
+    def reset_weight(self, weight_mean=0.8, weight_std=0.02):
+        """Resets weights to random values based on a normal distribution.
+
+        Args:
+            weight_mean (float, optional): Mean of the random weights. Default: 0.8.
+            weight_std (float, optional): Standard deviation of the random weights. Default: 0.02.
+        """
+        self.weight.normal_(weight_mean, weight_std)
+
+    def load_weight(self, target):
+        """Loads weights with the target tensor.
+
+        Args:
+            target (Tensor): The target tensor.
+        """
+        self.weight.copy_(target)
+
+    def forward(self, input):
+        r"""Performs the forward pass of the linear transformation on the input spike-wave.
+
+        Args:
+            input (Tensor): Input spike-wave of size :math:`(T, C_{{in}}, N)`.
+
+        Returns:
+            Tensor: Output spike-wave of size :math:`(T, C_{{out}}, N_{{out}})`.
+        """
+        return torch.matmul(input, self.weight.T)
+
+class Flatten(nn.Module):
+    r"""Flattens the input tensor into a 2D tensor.
+
+    Args:
+        start_dim (int, optional): The dimension to start flattening. Default: 1.
+        end_dim (int, optional): The dimension to end flattening. Default: -1.
+    """
+    def __init__(self, start_dim=1, end_dim=-1):
+        super(Flatten, self).__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, input):
+        return input.flatten(self.start_dim, self.end_dim)
+
 class Pooling(nn.Module):
     r"""Performs a 2D max-pooling over an input signal (spike-wave or potentials) composed of several input
     planes.
@@ -119,126 +190,88 @@ class Pooling(nn.Module):
         return sf.pooling(input, self.kernel_size, self.stride, self.padding)
 
 class STDP(nn.Module):
-    r"""Performs STDP learning rule over synapses of a convolutional layer based on the following formulation:
-
-    .. math::
-        \Delta W_{ij}=
-        \begin{cases}
-            a_{LTP}\times \left(W_{ij}-W_{LB}\right)\times \left(W_{UP}-W_{ij}\right) & \ \ \ t_j - t_i \leq 0,\\
-            a_{LTD}\times \left(W_{ij}-W_{LB}\right)\times \left(W_{UP}-W_{ij}\right) & \ \ \ t_j - t_i > 0,\\
-        \end{cases}
+    r"""Performs STDP learning rule over synapses of a convolutional or linear layer.
     
-    where :math:`i` and :math:`j` refer to the post- and pre-synaptic neurons, respectively,
-    :math:`\Delta w_{ij}` is the amount of weight change for the synapse connecting the two neurons,
-    and :math:`a_{LTP}`, and :math:`a_{LTD}` scale the magnitude of weight change. Besides,
-    :math:`\left(W_{ij}-W_{LB}\right)\times \left(W_{UP}-W_{ij}\right)` is a stabilizer term which
-    slowes down the weight change when the synaptic weight is close to the weight's lower (:math:`W_{LB}`)
-    and upper (:math:`W_{UB}`) bounds.
-
-    To create a STDP object, you need to provide:
-
-    * :attr:`conv_layer`: The convolutional layer on which the STDP should be applied.
-
-    * :attr:`learning_rate`: (:math:`a_{LTP}`, :math:`a_{LTD}`) rates. A single pair of floats or a list of pairs of floats. Each feature map has its own learning rates.
-
-    * :attr:`use_stabilizer`: Turns the stabilizer term on or off.
-
-    * :attr:`lower_bound` and :attr:`upper_bound`: Control the range of weights.
-
-    To apply STDP for a particular stimulus, you need to provide:
-    
-    * :attr:`input_spikes` and :attr:`potentials` that are the input spike-wave and corresponding potentials, respectively.
-
-    * :attr:`output_spikes` that is the output spike-wave.
-
-    * :attr:`winners` or :attr:`kwta` to find winners based on the earliest spike then the maximum potential.
-
-    * :attr:`inhibition_radius` to inhibit surrounding neurons (in all feature maps) within a particular radius.
-
-    Args:
-        conv_layer (snn.Convolution): Reference convolutional layer.
-        learning_rate (tuple of floats or list of tuples of floats): (LTP, LTD) rates for STDP.
-        use_stabilizer (boolean, optional): Turning stabilizer term on or off. Default: True
-        lower_bound (float, optional): Lower bound of the weight range. Default: 0
-        upper_bound (float, optional): Upper bound of the weight range. Default: 1
+    The rule is the same as previously defined, but the class adapts to both
+    convolutional and linear layers.
     """
-    def __init__(self, conv_layer, learning_rate, use_stabilizer = True, lower_bound = 0, upper_bound = 1):
+    def __init__(self, layer, learning_rate, use_stabilizer=True, lower_bound=0, upper_bound=1):
         super(STDP, self).__init__()
-        self.conv_layer = conv_layer
+        self.layer = layer  # Can be either convolutional or linear
         if isinstance(learning_rate, list):
             self.learning_rate = learning_rate
         else:
-            self.learning_rate = [learning_rate] * conv_layer.out_channels
-        for i in range(conv_layer.out_channels):
+            if isinstance(layer, Convolution):
+                self.learning_rate = [learning_rate] * layer.out_channels
+            else:  # For linear layers
+                self.learning_rate = [learning_rate] * layer.out_features
+
+        for i in range(len(self.learning_rate)):
             self.learning_rate[i] = (Parameter(torch.tensor([self.learning_rate[i][0]])),
-                            Parameter(torch.tensor([self.learning_rate[i][1]])))
+                                     Parameter(torch.tensor([self.learning_rate[i][1]])))
             self.register_parameter('ltp_' + str(i), self.learning_rate[i][0])
             self.register_parameter('ltd_' + str(i), self.learning_rate[i][1])
             self.learning_rate[i][0].requires_grad_(False)
             self.learning_rate[i][1].requires_grad_(False)
+        
         self.use_stabilizer = use_stabilizer
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
     def get_pre_post_ordering(self, input_spikes, output_spikes, winners):
-        r"""Computes the ordering of the input and output spikes with respect to the position of each winner and
-        returns them as a list of boolean tensors. True for pre-then-post (or concurrency) and False for post-then-pre.
-        Input and output tensors must be spike-waves.
+        if isinstance(self.layer, Convolution):
+            return self._get_pre_post_ordering_conv(input_spikes, output_spikes, winners)
+        else:
+            return self._get_pre_post_ordering_linear(input_spikes, output_spikes, winners)
 
-        Args:
-            input_spikes (Tensor): Input spike-wave
-            output_spikes (Tensor): Output spike-wave
-            winners (List of Tuples): List of winners. Each tuple denotes a winner in a form of a triplet (feature, row, column).
-
-        Returns:
-            List: pre-post ordering of spikes
-        """
-        # accumulating input and output spikes to get latencies
+    def _get_pre_post_ordering_conv(self, input_spikes, output_spikes, winners):
+        """STDP for convolutional layers."""
         input_latencies = torch.sum(input_spikes, dim=0)
         output_latencies = torch.sum(output_spikes, dim=0)
         result = []
         for winner in winners:
-            # generating repeated output tensor with the same size of the receptive field
-            out_tensor = torch.ones(*self.conv_layer.kernel_size, device=output_latencies.device) * output_latencies[winner]
-            # slicing input tensor with the same size of the receptive field centered around winner
-            # since there is no padding, there is no need to shift it to the center
-            in_tensor = input_latencies[:,winner[-2]:winner[-2]+self.conv_layer.kernel_size[-2],winner[-1]:winner[-1]+self.conv_layer.kernel_size[-1]]
-            result.append(torch.ge(in_tensor,out_tensor))
+            out_tensor = torch.ones(*self.layer.kernel_size, device=output_latencies.device) * output_latencies[winner]
+            in_tensor = input_latencies[:, winner[-2]:winner[-2]+self.layer.kernel_size[-2], winner[-1]:winner[-1]+self.layer.kernel_size[-1]]
+            result.append(torch.ge(in_tensor, out_tensor))
         return result
 
-    # simple STDP rule
-    # gets prepost pairings, winners, weights, and learning rates (all shoud be tensors)
-    def forward(self, input_spikes, potentials, output_spikes, winners=None, kwta = 1, inhibition_radius = 0):
+    def _get_pre_post_ordering_linear(self, input_spikes, output_spikes, winners):
+        """STDP for linear layers."""
+        input_latencies = torch.sum(input_spikes, dim=0)
+        output_latencies = torch.sum(output_spikes, dim=0)
+        result = []
+        for winner in winners:
+            out_tensor = output_latencies[winner]
+            in_tensor = input_latencies
+            result.append(in_tensor <= out_tensor)  # Simple pre-post ordering for linear layers
+        return result
+
+    def forward(self, input_spikes, potentials, output_spikes, winners=None, kwta=1, inhibition_radius=0):
         if winners is None:
             winners = sf.get_k_winners(potentials, kwta, inhibition_radius, output_spikes)
         pairings = self.get_pre_post_ordering(input_spikes, output_spikes, winners)
-        
-        lr = torch.zeros_like(self.conv_layer.weight)
-        for i in range(len(winners)):
-            f = winners[i][0]
-            lr[f] = torch.where(pairings[i], *(self.learning_rate[f]))
 
-        self.conv_layer.weight += lr * ((self.conv_layer.weight-self.lower_bound) * (self.upper_bound-self.conv_layer.weight) if self.use_stabilizer else 1)
-        self.conv_layer.weight.clamp_(self.lower_bound, self.upper_bound)
+        # Adjust learning rates for the layer type
+        lr = torch.zeros_like(self.layer.weight)
+        for i in range(len(winners)):
+            if isinstance(self.layer, Convolution):
+                f = winners[i][0]
+            else:
+                f = winners[i][0]  # For linear layers
+
+            lr[f] = torch.where(pairings[i], self.learning_rate[f][0], self.learning_rate[f][1])
+
+        # Update weights with stabilizer term if required
+        self.layer.weight += lr * ((self.layer.weight - self.lower_bound) * (self.upper_bound - self.layer.weight) if self.use_stabilizer else 1)
+        self.layer.weight.clamp_(self.lower_bound, self.upper_bound)
 
     def update_learning_rate(self, feature, ap, an):
-        r"""Updates learning rate for a specific feature map.
-
-        Args:
-            feature (int): The target feature.
-            ap (float): LTP rate.
-            an (float): LTD rate.
-        """
+        """Updates learning rate for a specific feature."""
         self.learning_rate[feature][0][0] = ap
         self.learning_rate[feature][1][0] = an
 
     def update_all_learning_rate(self, ap, an):
-        r"""Updates learning rates of all the feature maps to a same value.
-
-        Args:
-            ap (float): LTP rate.
-            an (float): LTD rate.
-        """
-        for feature in range(self.conv_layer.out_channels):
+        """Updates learning rates of all the features to the same value."""
+        for feature in range(len(self.learning_rate)):
             self.learning_rate[feature][0][0] = ap
             self.learning_rate[feature][1][0] = an
