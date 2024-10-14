5a490617a912042dc14206d904523d0c004567d6
aidinattar <attaraidin@gmail.com>
2024-10-14 23:11:43 +0200

    add deeper models

--- surrogate_grad.py
+++ surrogate_grad.py
@@ -13,6 +13,7 @@
 # %%
 import os
 import torch
+import tonic
 import argparse
 import torch.nn as nn
 import snntorch as snn
@@ -26,6 +27,7 @@ from torchvision import datasets, transforms
 from tqdm import tqdm
 import torch.nn.functional as F
 from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, recall_score, precision_score
+import torch.cuda.amp as amp
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Suppress TensorFlow logging
 from torch.utils.tensorboard import SummaryWriter
@@ -165,6 +167,129 @@ class ConvSNN(SNN):
 
         return spk3, mem3
 
+class ConvSNN2(SNN):
+    """
+    Convolutional Spiking Neural Network model using surrogate gradient
+    to process neuromorphic data.
+    """
+    def __init__(self, beta, spike_grad, kernel_size):
+        super().__init__()
+
+        # Initialize layers
+        self.conv1 = nn.Conv2d(2, 12, kernel_size)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.conv2 = nn.Conv2d(12, 32, kernel_size)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.fc1 = nn.Linear(32*5*5, 10)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
+
+    def forward(self, x):
+        # Initialize hidden states and outputs at t=0
+        # mem1 = self.lif1.init_leaky()
+        # mem2 = self.lif2.init_leaky()
+        # mem3 = self.lif3.init_leaky()
+
+        cur1 = F.max_pool2d(self.conv1(x), 2, 2)
+        spk1 = self.lif1(cur1)
+
+        cur2 = F.max_pool2d(self.conv2(spk1), 2, 2)
+        spk2 = self.lif2(cur2)
+
+        cur3 = self.fc1(spk2.view(x.size(0), -1))
+        spk3 = self.lif3(cur3)
+
+        return spk3
+    
+
+class DeepConvSNN(SNN):
+    """
+    Deep Convolutional Spiking Neural Network model using surrogate gradient.
+    """
+    def __init__(self, beta, spike_grad, kernel_size):
+        super().__init__()
+
+        # Initialize layers
+        self.conv1 = nn.Conv2d(2, 30, 5)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.conv2 = nn.Conv2d(30, 150, 3)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.conv3 = nn.Conv2d(150, 250, 3)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.conv4 = nn.Conv2d(250, 250, 3)
+        self.lif4 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.fc1 = nn.Linear(250 * 5 * 5, 10)
+        self.lif5 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
+
+    def forward(self, x):
+        # Pad the input to avoid edge effects
+        x = F.pad(x, (2, 2, 2, 2))
+
+        x = F.max_pool2d(self.conv1(x), 2, 2)
+        x = self.lif1(x)
+        x = F.pad(x, (1, 1, 1, 1))
+
+        x = F.max_pool2d(self.conv2(x), 2, 2)
+        x = self.lif2(x)
+        x = F.pad(x, (1, 1, 1, 1))
+
+        x = F.max_pool2d(self.conv3(x), 3, 3)
+        x = self.lif3(x)
+        x = F.pad(x, (2, 2, 2, 2))
+
+        x = F.max_pool2d(self.conv4(x), 2, 2)
+        x = self.lif4(x)
+
+        x = self.fc1(x.view(x.size(0), -1))
+        x = self.lif5(x)
+
+        return x
+
+
+class DeepConvSNN2(SNN):
+    """
+    Deep Convolutional Spiking Neural Network model using surrogate gradient
+    to process neuromorphic data.
+    """
+    def __init__(self, beta, spike_grad, kernel_size):
+        super().__init__()
+
+        # Initialize layers
+        self.conv1 = nn.Conv2d(2, 30, 5)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.conv2 = nn.Conv2d(30, 250, 3)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.conv3 = nn.Conv2d(250, 200, 5)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True)
+        self.fc1 = nn.Linear(200 * 5 * 5, 10)
+        self.lif5 = snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
+
+    def forward(self, x):
+        # Initialize hidden states and outputs at t=0
+        # mem1 = self.lif1.init_leaky()
+        # mem2 = self.lif2.init_leaky()
+        # mem3 = self.lif3.init_leaky()
+        # mem4 = self.lif4.init_leaky()
+        # mem5 = self.lif5.init_leaky()
+
+        x = F.pad(x, (2, 2, 2, 2))
+
+        x = F.max_pool2d(self.conv1(x), 2, 2)
+        x = self.lif1(x)
+        x = F.pad(x, (1, 1, 1, 1))
+
+        x = F.max_pool2d(self.conv2(x), 3, 3)
+        x = self.lif2(x)
+        x = F.pad(x, (1, 1, 1, 1))
+
+        x = F.max_pool2d(self.conv3(x), 3, 3)
+        x = self.lif3(x)
+        x = F.pad(x, (2, 2, 2, 2))
+
+        x = self.fc1(x.view(x.size(0), -1))
+        x = self.lif5(x)
+
+        return x
+
 # %%
 def forward_pass(model, data, num_steps):
     """
@@ -178,6 +303,24 @@ def forward_pass(model, data, num_steps):
 
     for _ in range(num_steps):
         spk_out, mem_out = model(data)
+        spk_rec.append(spk_out)
+        mem_rec.append(mem_out)
+
+    return torch.stack(spk_rec), torch.stack(mem_rec)
+
+# %%
+def forward_pass2(model, data):
+    """
+    Perform a forward pass through the network.
+    """
+    mem_rec = []
+    spk_rec = []
+    
+    # Reset the internal states of the model
+    utils.reset(model)
+
+    for step in range(data.size(0)):
+        spk_out, mem_out = model(data[step])
         spk_rec.append(spk_out)
         mem_rec.append(mem_out)
 
@@ -220,19 +363,33 @@ def load_data(batch_size, dataset):
     elif dataset == "cifar10":
         train_dataset = datasets.CIFAR10(root="data", train=True, transform=transform, download=True)
         test_dataset = datasets.CIFAR10(root="data", train=False, transform=transform, download=True)
+    elif dataset == "nmnist":
+        transform = tonic.transforms.Compose([
+            tonic.transforms.Denoise(filter_time=3000),
+            # tonic.transforms.ToFrame(sensor_size=tonic.datasets.NMNIST.sensor_size, time_window=1000),
+            tonic.transforms.ToFrame(sensor_size=tonic.datasets.NMNIST.sensor_size, n_time_bins=15),
+        ])
+        train_dataset = tonic.datasets.NMNIST(save_to='./data', transform=transform, train=True)
+        test_dataset = tonic.datasets.NMNIST(save_to='./data', transform=transform, train=False)
+        train_dataset = tonic.DiskCachedDataset(train_dataset, cache_path='./cache/nmnist/train')
+        test_dataset = tonic.DiskCachedDataset(test_dataset, cache_path='./cache/nmnist/test')
     else:
         raise ValueError("Invalid dataset specified. Please use 'mnist', 'fashion', or 'cifar10'.")
 
     # Create the DataLoader objects
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    if dataset == "nmnist":
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=tonic.collation.PadTensors(batch_first=False))
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=tonic.collation.PadTensors(batch_first=False))
+    else:
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
 
 # %%
-def train(model, train_loader, optimizer, loss_fn, num_steps, device, writer = None, epoch = 0):
+def train(model, train_loader, optimizer, loss_fn, num_steps, device, writer=None, epoch=0):
     """
-    Train the model on the training dataset.
+    Train the model on the training dataset using mixed precision.
     """
     # Set the model to training mode
     model.train()
@@ -240,77 +397,102 @@ def train(model, train_loader, optimizer, loss_fn, num_steps, device, writer = N
     # Initialize the loss and accuracy
     total_loss = 0.0
     total_correct = 0
-
     batch_idx = 0
-    counter = 0
+    accumulation_steps = 4
+
+    # Mixed precision scaler
+    scaler = amp.GradScaler()
+
+    # Learning rate scheduler
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=20)
 
     # Iterate over the training dataset
-    for inputs, targets in tqdm(train_loader, desc="Training", leave=False):
+    iterator = tqdm(train_loader, desc=f'Epoch {epoch + 1}', unit='batch', position=1, leave=False, dynamic_ncols=True, total=len(train_loader), initial=0, ascii=True)
+    for i, (inputs, targets) in enumerate(iterator):
         # Move the data to the specified device
         inputs, targets = inputs.to(device), targets.to(device)
 
-        # Forward pass
-        outputs, _ = forward_pass(model, inputs, num_steps)
-        loss = loss_fn(outputs, targets)
-
-        # print("Accuracy: ", batch_accuracy(train_loader, model, num_steps, device))
-
-        # Zero the gradients
+        # Zero gradients
         optimizer.zero_grad()
-        # Backward pass
-        loss.backward()
-        optimizer.step()
+
+        # Forward pass inside mixed precision context
+        with amp.autocast():
+            if model.__class__.__name__ in ['ConvSNN2', 'DeepConvSNN', 'DeepConvSNN2']:
+                outputs, _ = forward_pass2(model, inputs)
+            else:
+                outputs, _ = forward_pass(model, inputs, num_steps)
+
+            # Compute loss
+            loss = loss_fn(outputs, targets)
+
+        # Backward pass with scaled gradients
+        scaler.scale(loss).backward()
+
+        # Accumulate gradients
+        if (i + 1) % accumulation_steps == 0:
+            scaler.step(optimizer)  # Unscales the gradients and performs optimizer step
+            scaler.update()  # Updates the scaler for next iteration
+            optimizer.zero_grad()
 
         # Compute the loss and accuracy
         batch_loss = loss.item()
-        batch_correct = SF.accuracy_rate(outputs, targets) * outputs.size(1)
+        batch_correct = SF.accuracy_rate(outputs, targets)
         total_loss += batch_loss
-        total_correct += batch_correct
+        total_correct += batch_correct * inputs.size(0)
+
+        iterator.set_postfix(loss=batch_loss, accuracy=batch_correct)
 
         # Log the loss and accuracy
-        # print(f"Loss: {batch_loss:.4f}, Accuracy: {batch_correct / inputs.size(0):.4f}")
         if writer is not None:
-            writer.add_scalar("Loss/TrainBatch", batch_loss, len(train_loader) * epoch + batch_idx)
-            writer.add_scalar("Accuracy/TrainBatch", batch_correct / inputs.size(0), len(train_loader) * epoch + batch_idx)
+            writer.add_scalar("Loss_Iteration/Train", batch_loss, len(train_loader) * epoch + batch_idx)
+            writer.add_scalar("Accuracy_Iteration/Train", batch_correct, len(train_loader) * epoch + batch_idx)
         batch_idx += 1
 
-    return total_loss / len(train_loader), total_correct / len(train_loader.dataset)
+        # Step the learning rate scheduler
+        scheduler.step(batch_loss)
 
+    return total_loss / len(train_loader), total_correct / len(train_loader.dataset)
 # %%
-def test(model, test_loader, loss_fn, num_steps, device, writer = None, epoch = 0):
+def test(model, test_loader, loss_fn, num_steps, device, writer=None, epoch=0):
     """
-    Test the model on the testing dataset.
+    Test the model on the testing dataset using mixed precision.
     """
-    # Set the model to evaluation
+    # Set the model to evaluation mode
     model.eval()
 
     # Initialize the loss and accuracy
     total_loss = 0.0
     total_correct = 0
 
-    # Disable gradient computation
+    # Disable gradient computation and use mixed precision
     with torch.no_grad():
         batch_idx = 0
+
         # Iterate over the testing dataset
         for inputs, targets in tqdm(test_loader, desc="Testing", leave=False):
             # Move the data to the specified device
             inputs, targets = inputs.to(device), targets.to(device)
 
-            # Forward pass
-            outputs, _ = forward_pass(model, inputs, num_steps)
-            loss = loss_fn(outputs, targets)
+            # Forward pass with mixed precision
+            with amp.autocast():
+                if model.__class__.__name__ in ['ConvSNN2', 'DeepConvSNN', 'DeepConvSNN2']:
+                    outputs, _ = forward_pass2(model, inputs)
+                else:
+                    outputs, _ = forward_pass(model, inputs, num_steps)
+
+                # Compute the loss
+                loss = loss_fn(outputs, targets)
 
             # Compute the loss and accuracy
             batch_loss = loss.item()
-            batch_correct = SF.accuracy_rate(outputs, targets) * outputs.size(1)
+            batch_correct = SF.accuracy_rate(outputs, targets)
             total_loss += batch_loss
-            total_correct += batch_correct
-            
+            total_correct += batch_correct * inputs.size(0)
+
             # Log the loss and accuracy
-            # print(f"Loss: {batch_loss:.4f}, Accuracy: {batch_correct / inputs.size(0):.4f}")
             if writer is not None:
-                writer.add_scalar("Loss/TestBatch", batch_loss, len(test_loader) * epoch + batch_idx)
-                writer.add_scalar("Accuracy/TestBatch", batch_correct / inputs.size(0), len(test_loader) * epoch + batch_idx)
+                writer.add_scalar("Loss_Iteration/Test", batch_loss, len(test_loader) * epoch + batch_idx)
+                writer.add_scalar("Accuracy_Iteration/Test", batch_correct, len(test_loader) * epoch + batch_idx)
 
             batch_idx += 1
 
@@ -369,14 +551,13 @@ def main():
     parser.add_argument("--loss_fn", type=str, default="crossentropy", help="Loss function to use for training.")
     parser.add_argument("--weight_decay", type=float, default=0, help="Weight decay for the optimizer.")
     parser.add_argument("--beta", type=float, default=0.5, help="Beta value for the LIF neuron.")
-    parser.add_argument("--spike_grad", type=str, default="fast_sigmoid", help="Spike gradient to use for training.")
+    parser.add_argument("--spike_grad", type=str, default="atan", help="Spike gradient to use for training.")
     parser.add_argument("--dropout", type=float, default=0.2, help="Dropout rate for the network.")
     parser.add_argument("--kernel_size", type=int, default=5, help="Kernel size for the convolutional layers.")
     parser.add_argument("--n_filters", type=int, default=32, help="Number of filters for the convolutional layers.")
     parser.add_argument("--n_hidden", type=int, default=128, help="Number of hidden units in the fully connected layer.")
     parser.add_argument("--n_out", type=int, default=10, help="Number of output units in the fully connected layer.")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use for training.")
-    parser.add_argument("--log_dir", type=str, default="runs_surrogate", help="Directory to save logs to.")
     parser.add_argument("--save_dir", type=str, default="saves", help="Directory to save model checkpoints to.")
     parser.add_argument("--plot", action="store_true", help="Plot the spikes and weights of the network.")
     parser.add_argument("--metrics", action="store_true", help="Compute the metrics at the end of the epoch.")
@@ -396,6 +577,8 @@ def main():
     for arg in vars(args):
         print(f"\t{arg}: {getattr(args, arg)}")
     print("############################################")
+
+    log_dir = f"runs/surrogate/{args.model}/{args.dataset}"
 
     # %%
     if not os.path.isdir("models"):
@@ -420,12 +603,21 @@ def main():
         model = fcSNN(args.beta, spike_grad, 28*28, args.n_hidden, args.n_out)
     elif args.model == "convsnn":
         model = ConvSNN(args.beta, spike_grad, args.kernel_size)
+    elif args.model == "convsnn2":
+        model = ConvSNN2(args.beta, spike_grad, args.kernel_size)
+    elif args.model == "deepconvsnn":
+        model = DeepConvSNN(args.beta, spike_grad, args.kernel_size)
+    elif args.model == "deepconvsnn2":
+        model = DeepConvSNN2(args.beta, spike_grad, args.kernel_size)
     else:
         raise ValueError("Invalid model specified. Please use 'snn'.")
     
     # %%
     # Move the model to the specified device
     model.to(args.device)
+
+    # Print model
+    print(model)
 
     # %%
     # Define the optimizer
@@ -440,6 +632,8 @@ def main():
     # Define the loss function
     if args.loss_fn == "crossentropy":
         loss_fn = SF.ce_rate_loss()
+    elif args.loss_fn == "mse":
+        loss_fn = SF.mse_count_loss(correct_rate=0.8, incorrect_rate=0.2)
     else:
         raise ValueError("Invalid loss function specified. Please use 'crossentropy'.")
     
@@ -452,7 +646,7 @@ def main():
 
     # Initialize the TensorBoard writer
     if args.tensorboard:
-        writer = SummaryWriter(log_dir=args.log_dir)
+        writer = SummaryWriter(log_dir=log_dir)
     else:
         writer = None
 
@@ -462,7 +656,7 @@ def main():
 
     # %%
     # Train the model
-    iterator = tqdm(range(args.epochs), desc="Epoch")
+    iterator = tqdm(range(args.epochs), desc='Training', unit='epoch', position=0, leave=True, dynamic_ncols=True, total=args.epochs, initial=0, ascii=True)
     for epoch in iterator:
         # Train the model
         train_loss, train_acc = train(model, train_loader, optimizer, loss_fn, args.num_steps, args.device, writer, epoch)
